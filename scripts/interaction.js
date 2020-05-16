32491cc6af5e4a06fcc23a8535c93b44fd6b4ee8
unknown <theartofdeception09@gmail.com>
2020-05-16 21:32:55 +0530

    Navigation Bar, hero Area

---
+++ scripts/interaction.js
@@ -0,0 +1,7 @@
+function toggleMenu(){
+    var menu = document.querySelector(".navigation .menu").classList.toggle("active");
+}
+
+// video contorls
+document.querySelector(".hero-area video").playbackRate = 0.6;
+    
