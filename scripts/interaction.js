98f600bc74a2007a2e806f2c8c7a85dc2cb23dd6
unknown <theartofdeception09@gmail.com>
2020-05-19 12:16:43 +0530

    Cart Popup

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -12,15 +12,26 @@ var plus = document.querySelector(".medicine-details .product-information .quant
 var minus = document.querySelector(".medicine-details .product-information .quantity .minus");
 var quantity = document.querySelector(".medicine-details .product-information .quantity input");
 
-quantity.value = 1;
+if(quantity && plus && minus){
+    quantity.value = 1;
 
 
-plus.addEventListener('click',increase_quantity);
-minus.addEventListener('click',decrease_quantity);
+    plus.addEventListener('click',increase_quantity);
+    minus.addEventListener('click',decrease_quantity);
 
-function increase_quantity(){
-    quantity.value = parseInt(quantity.value) + 1;
-}
-function decrease_quantity(){
-    if(parseInt(quantity.value) > 1) quantity.value = parseInt(quantity.value) - 1
-}
+    function increase_quantity(){
+        quantity.value = parseInt(quantity.value) + 1;
+    }
+    function decrease_quantity(){
+        if(parseInt(quantity.value) > 1) quantity.value = parseInt(quantity.value) - 1
+    }
+} 
+
+
+// cart popup open/close
+var cart_button = document.querySelector(".navigation .options .cart-btn span");
+var cart_popup = document.querySelector(".navigation .options .cart-btn .cart-popup");
+
+cart_button.addEventListener('click',function(){
+    cart_popup.classList.toggle("active");
+})
