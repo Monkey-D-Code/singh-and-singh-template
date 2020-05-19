9b99217895fcbc114d139b582f31fb6f029cdb4f
unknown <theartofdeception09@gmail.com>
2020-05-19 10:51:38 +0530

    Quantity change

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -3,8 +3,24 @@ function toggleMenu(){
 }
 
 // video contorls
-document.querySelector(".hero-area video").playbackRate = 0.6;
+// document.querySelector(".hero-area video").playbackRate = 0.6;
 
 
 
-    
+// Medicine Quantity Change
+var plus = document.querySelector(".medicine-details .product-information .quantity .plus");
+var minus = document.querySelector(".medicine-details .product-information .quantity .minus");
+var quantity = document.querySelector(".medicine-details .product-information .quantity input");
+
+quantity.value = 1;
+
+
+plus.addEventListener('click',increase_quantity);
+minus.addEventListener('click',decrease_quantity);
+
+function increase_quantity(){
+    quantity.value = parseInt(quantity.value) + 1;
+}
+function decrease_quantity(){
+    if(parseInt(quantity.value) > 1) quantity.value = parseInt(quantity.value) - 1
+}
