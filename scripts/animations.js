4d3215c425fe3b68813d0c62e272b441ba98b056
unknown <theartofdeception09@gmail.com>
2020-05-22 13:42:46 +0530

    Nav bar micro adjustment, GSAP added

---
+++ scripts/animations.js
@@ -0,0 +1,17 @@
+// ********************* HOME PAGE *************************************
+gsap.from(".navigation-2 .search-bar .brand img" , {
+    duration : 2,
+    width : "50px",
+    ease : "elastic"
+});
+gsap.from(".navigation-2 .search-bar .search" , {
+    duration : 1,
+    y : "-100px",
+    opacity : 0,
+    ease : "ease",
+    delay : 1,
+});
+
+
+
+// **********************************************************************
