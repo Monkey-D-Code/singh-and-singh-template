beaed60bc430cd2eb1f9c294c19485dd9b891772
unknown <theartofdeception09@gmail.com>
2020-05-24 16:26:03 +0530

    Slides, what we do, top products animated scroll

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,13 +1,18 @@
 // ************************ SIDE MENU *********************************
-const sideMenuTimeline = gsap.timeline();
+const sideMenuTimeline = gsap.timeline({paused : true});
 sideMenuTimeline.to(".navigation-2 .side-menu",{
-    x : "0px",
+    right : "-1px",
+    duration : 0.05,
+    ease : "ease",
+}).from(".navigation-2 .side-menu ul li" , {
+    opacity : 0,
+    x : "250px",
+    duration : 0.06,
+    stagger : 0.04,
 })
-
-
 // *********************************************************************
 
-// ********************* HOME PAGE *************************************
+// ****************** NAVIGATION MENU **********************************
 gsap.from(".navigation-2 .search-bar .brand img" , {
     duration : 2,
     width : "50px",
@@ -21,6 +26,11 @@ gsap.from(".navigation-2 .search-bar .search" , {
     delay : 1,
 });
 
+// ***********************************************************************
+
+// ********************* HOME PAGE *************************************
+
+// SLIDER
 var no_of_slides = document.querySelectorAll(".hero-slider .slide-container .slide").length;
 var slideTimeline = gsap.timeline({delay  :1 ,repeat : -1 , yoyo : true});
 var slide_width = document.querySelector(".hero-slider").clientWidth;
@@ -31,8 +41,43 @@ if(no_of_slides){
         duration : (no_of_slides * 3),
         ease : "none",
     });
-    
-    slideTimeline.play();
+
+}
+
+// WHAT WE DO
+const whatWeDo = document.querySelector(".our-services");
+const whatWeDoTimeline = gsap.timeline({paused : true});
+if(whatWeDo){
+    whatWeDoTimeline.from(".our-services .lead-img",{
+        opacity : 0,
+        duration : 0.6,
+        ease : "ease",
+    }).from(".our-services .list .service",{
+        scale : 0,
+        duration : 0.3,
+        stagger : 0.2
+    },"-=0.3")
+}
+
+// TOP RATED PRODUCTS
+const topRatedProducts = document.querySelector(".top-rated-products");
+const topRatedProductsTimeline = gsap.timeline({paused : true});
+if(topRatedProducts){
+    topRatedProductsTimeline.from(".top-rated-products svg",{
+        scaleX : 0,
+        transformOrigin : "top left",
+        duration : 0.8,
+        ease : Power1.ease,
+    }).from(".top-rated-products .heading",{
+        scale : 0,
+        duration : 0.6,
+        ease : "elastic",
+    },"-=0.6").from(".top-rated-products .product-list .single-product",{
+        scale : 0,
+        duration : 0.4,
+        ease : Power2.ease,
+        stagger : 0.2
+    },"-=0.1")
 }
 
 
--- scripts/interaction.js
+++ scripts/interaction.js
@@ -1,11 +1,52 @@
-function toggleMenu(){
-    var menu = document.querySelector(".navigation-2 .side-menu").classList.toggle("active");
+// *********************** SIDE MENU INTERACTION **************************
+var hamburger = document.querySelector(".navigation-2 .side-menu .hamburger");
+if(hamburger){
+    hamburger.addEventListener("click",()=>{
+        if(sideMenuTimeline.progress() === 0){
+            sideMenuTimeline.play();
+        }else if(sideMenuTimeline.progress() ===  1){
+            sideMenuTimeline.reverse();
+        }
+        
+    })
+}
+// ****************************************************************************
+
+
+// ******************************* HOME PAGE ***********************************
+// WHAT WE DO
+if(whatWeDo){
+    const options = {
+        rootMargin : screen.width > 768 ? "-250px" : "-120px",
+    };
+    const whatWeDoObserver = new IntersectionObserver((entries)=>{
+        if(entries[0].isIntersecting){
+            whatWeDoTimeline.play();
+        } else{
+            whatWeDoTimeline.reverse();
+        }
+    } , options);   
+    whatWeDoObserver.observe(whatWeDo);
 }
 
-// video contorls
-// document.querySelector(".hero-area video").playbackRate = 0.6;
+// TOP RATED PRODUCTS
+if(topRatedProducts){
+    const options = {
+        rootMargin : screen.width > 768 ? "-200px" : "-130px",
+    };
+    const topRatedProductsObserver = new IntersectionObserver((entries)=>{
+        if(entries[0].isIntersecting){
+            topRatedProductsTimeline.play();
+        } else{
+            topRatedProductsTimeline.reverse();
+        }
+    } , options);   
+    topRatedProductsObserver.observe(topRatedProducts);
+}
 
 
+
+// *******************************************************************************
 
 // Medicine Quantity Change
 var plus = document.querySelector(".medicine-details .product-information .quantity .plus");
